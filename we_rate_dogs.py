0aad93362bdb219586ffd1fe24075f14219e3824
Ale <asierra21@gmail.com>
2020-12-26 14:05:51 -0100

    handling exceptions saving to tweet_json.txt

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -6,7 +6,7 @@ import seaborn as sb
 import requests
 import os
 import json
-#import tweepy
+import tweepy
 
 #read csv
 twitter_archive = pd.read_csv('twitter-archive-enhanced.csv')
@@ -23,7 +23,6 @@ if not os.path.exists(url.split('/')[-1]):
 image_predictions = pd.read_csv('image-predictions.tsv', sep='\t')
 
 #twitter api information
-import tweepy
 
 consumer_key = '0Bfm4ndyhQn7KySsuh51Vnukm'
 consumer_secret = 'gqTSd4AMo69HT1AnaXjhz1IXgF5BFRCOtYWnYUyufRChtMhRof'
@@ -36,19 +35,30 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 #read retweet and favorite from api by id
-
+if not os.path.exists('twitter_json.txt'):
 #create dictionary to build file
-twitter_json = []
-for id in twitter_archive['tweet_id'][0:3]:
-    tweet = api.get_status(id, tweet_mode='extended')
-    retweet_count = tweet.retweet_count
-    favorite_count = tweet.favorite_count
+    twitter_json = []
+    for id in twitter_archive['tweet_id'][0:10]:
+        try:
+            tweet = api.get_status(id, tweet_mode='extended')
+            retweet_count = tweet.retweet_count
+            favorite_count = tweet.favorite_count
 
-    # Append to list of dictionaries
-    twitter_json.append({'retweet_count': retweet_count,
-                        'favorite_count': favorite_count,
-                        'tweet_id': id})
-        
-twitter_df = pd.DataFrame(twitter_json, columns = ['tweet_id', 'retweet_count', 'favorite_count'])
+            # Append to list of dictionaries
+            twitter_json.append({'retweet_count': retweet_count,
+                                'favorite_count': favorite_count,
+                                'tweet_id': id})
+        except tweepy.error.TweepError:
+            print('Missing id')
 
-print(twitter_df.head())
+    #write twitter_df to a json file
+    with open('tweeter_json.txt', 'w') as outfile:
+        json.dump(twitter_json, outfile)
+
+with open('tweeter_json.txt') as json_file:
+    twitter_json = json.load(json_file)
+
+
+
+twitter_json = pd.DataFrame(twitter_json, columns = ['tweet_id', 'retweet_count', 'favorite_count'])
+print(twitter_json.head())
