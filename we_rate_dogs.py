336fb5cf574d863730d4f723af836461d56c5cb5
Ale <asierra21@gmail.com>
2020-12-26 13:18:16 -0100

    get data from twitter api by tweet_id. try and except blocks not included and needed

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -10,7 +10,6 @@ import json
 
 #read csv
 twitter_archive = pd.read_csv('twitter-archive-enhanced.csv')
-print(twitter_archive.head())
 
 #download file from web
 url = 'https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv'
@@ -22,7 +21,6 @@ if not os.path.exists(url.split('/')[-1]):
         file.write(response.content)
 
 image_predictions = pd.read_csv('image-predictions.tsv', sep='\t')
-print(image_predictions.head())
 
 #twitter api information
 import tweepy
@@ -36,3 +34,21 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+#read retweet and favorite from api by id
+
+#create dictionary to build file
+twitter_json = []
+for id in twitter_archive['tweet_id'][0:3]:
+    tweet = api.get_status(id, tweet_mode='extended')
+    retweet_count = tweet.retweet_count
+    favorite_count = tweet.favorite_count
+
+    # Append to list of dictionaries
+    twitter_json.append({'retweet_count': retweet_count,
+                        'favorite_count': favorite_count,
+                        'tweet_id': id})
+        
+twitter_df = pd.DataFrame(twitter_json, columns = ['tweet_id', 'retweet_count', 'favorite_count'])
+
+print(twitter_df.head())
