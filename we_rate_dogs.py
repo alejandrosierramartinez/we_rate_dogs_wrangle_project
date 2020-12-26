7fcf553442c0468a74b52c783d32210709fa2a4e
Ale <asierra21@gmail.com>
2020-12-26 11:34:49 -0100

    only read once conditional statement

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -13,9 +13,13 @@ twitter_archive = pd.read_csv('twitter-archive-enhanced.csv')
 
 #download file from web
 url = 'https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv'
-response = requests.get(url)
 
-with open(os.path.join(url.split('/')[-1]), mode = 'wb') as file: #add encoding
-    file.write(response.content)
+if not os.path.exists(url.split('/')[-1]):
+    response = requests.get(url)
+
+    with open(os.path.join(url.split('/')[-1]), mode = 'wb') as file: #add encoding
+        file.write(response.content)
 
 print(os.listdir())
+
+#twitter api information
