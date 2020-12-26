da452fa8606a4c1310b74dcabe050fe0940280fd
Ale <asierra21@gmail.com>
2020-12-26 12:33:28 -0100

    load tsv file

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -10,6 +10,7 @@ import json
 
 #read csv
 twitter_archive = pd.read_csv('twitter-archive-enhanced.csv')
+print(twitter_archive.head())
 
 #download file from web
 url = 'https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv'
@@ -20,6 +21,18 @@ if not os.path.exists(url.split('/')[-1]):
     with open(os.path.join(url.split('/')[-1]), mode = 'wb') as file: #add encoding
         file.write(response.content)
 
-print(os.listdir())
+image_predictions = pd.read_csv('image-predictions.tsv', sep='\t')
+print(image_predictions.head())
 
 #twitter api information
+import tweepy
+
+consumer_key = '0Bfm4ndyhQn7KySsuh51Vnukm'
+consumer_secret = 'gqTSd4AMo69HT1AnaXjhz1IXgF5BFRCOtYWnYUyufRChtMhRof'
+access_token = '524802061-TeSt8iwdGD32eUZX0AfMKGmce6mVM7NERYnQE7KM'
+access_secret = '0V5dTYz0dvY8aoFbylbQGj8lxPmY1T5lJPayRpVdBQTxr'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
